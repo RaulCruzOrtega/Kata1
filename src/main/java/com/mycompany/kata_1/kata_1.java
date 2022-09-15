33eb88a549c44c18fad697a39411f17ca66bfaaa
rauli <rauli@10.195.13.177>
2022-09-15 10:18:27 +0100

    Kata1 varsión2

--- src/main/java/com/mycompany/kata_1/kata_1.java
+++ src/main/java/com/mycompany/kata_1/kata_1.java
@@ -1,11 +1,15 @@
 package com.mycompany.kata_1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class kata_1 {
     
     public static void main(String[] args){
-        Person person = new Person("Juaquin", new Date(102, 9, 5));
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(2002, 6, 9);
+        Person person = new Person("Juaquin", date);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
     
